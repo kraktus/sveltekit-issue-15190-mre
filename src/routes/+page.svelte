5b6c208fe175eff3f66083883ba5efc12e97b568
kraktus <kraktus@users.noreply.github.com>
2026-01-22 13:01:11 +0100

    init commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+    import favicon from '$lib/assets/favicon.svg';
+    export const ssr = false;
+
+    let { children } = $props();
+</script>
+
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
